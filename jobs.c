4d570b2682e37f64ee68798e5b9726f48b25b532
Denis Parfenov <tempoden@yandex.ru>
2019-05-15 19:04:43 +0700

    Added more comments in jobs.c and shell.h

--- jobs.c
+++ jobs.c
@@ -5,30 +5,39 @@
 #include <signal.h>
 #include <wait.h>
 
+/*Active jobs amount*/
 size_t job_count = 0;
 
 int push_job(job_t* new_job){
+	/*If job is new*/
 	if(new_job->index == ((size_t) -1)){
 		new_job->index = job_count++;
 		memcpy(&jobs[job_count - 1], new_job, sizeof(job_t));
 	}else{
+		/*If jobs was in jobs[] before*/
 		memcpy(&jobs[new_job->index], new_job, sizeof(job_t));
 		if(new_job->index >= job_count){
 			job_count++;
 		}
 	}
+	/*NOTIFICATIONS*/
+	
 	if (new_job->status == RUNNING) {
+		/*If job was started in background with &*/
 		printf("[%ld]\t%d\n", new_job->index+1, new_job->job_pids[new_job->size-1]);
 	} else {
+		/*If job was stopped (whith ^Z or by signal)*/
 		printf("\n[%ld]+\tStopped\t\t%s\n", new_job->index+1, jobs[new_job->index].job_title);
 	}
 	return 0;
 }
 
 int pop_job(job_t* dest){
+	/*If there are no jobs, or lats one just terminated return -1*/
 	if(job_count == 0 || check_for_termination( &jobs[job_count-1])){
 		return -1;
 	}
+	/*Else pop job from jobs[] and write it to dest*/
 	job_count--;
 	printf("%s\n", jobs[job_count].job_title);
 	memcpy(dest, &jobs[job_count], sizeof(job_t));
@@ -41,13 +50,18 @@ int pop_job(job_t* dest){
 int check_for_termination(job_t* job){
 	int stat;
 	pid_t pid = waitpid(job->job_pids[job->size-1], &stat, WNOHANG);
+	/*If job was stopped in background and finished
+	there was abnormal termination	*/
 	if(pid>0 && ((WIFEXITED(stat)) || (WIFSIGNALED(stat)))){
 		printf("\n[%ld]+\tTerminated\t\t%s\n", job->index+1, job->job_title);
+		/*kill all group*/
 		kill(-job->job_pids[0], SIGKILL);
+		/*and then wait for them*/
 		size_t i;
 		for (i = 0; i < job->size-1; i++) {
 			waitpid(job->job_pids[i], 0, 0);
 		}
+		/*clean obj*/
 		memset(job, 0, sizeof(job_t));
 		return 1;
 	}else{
@@ -56,9 +70,11 @@ int check_for_termination(job_t* job){
 }
 
 int get_job(job_t* dest, int index) {
+	/*check for incorrect index*/
 	if (index > job_count || index <= 0) {
 		return -1;
 	}
+	/*check if needed job is still "alive"*/
 	if(check_for_termination(&jobs[index-1])){
 		return -1;
 	}
@@ -94,20 +110,23 @@ void init_title(char* title, char **cmdargs, char flag) {
 void wait_job(job_t* job) {	
 	int stat = 0;
 CHECK:
+	/*Wait for the last process in pipeline*/
 	if (waitpid(job->job_pids[job->size-1], &stat, WUNTRACED) != -1) {
-		//If Ctrl+Z (default) is pressed, move fg processes to bg
-		//  && (SIGTSTP == WSTOPSIG(stat) || SIGSTOP == WSTOPSIG(stat))
+		/*If Ctrl+Z (default) is pressed, move fg processes to bg*/
+		/*  && (SIGTSTP == WSTOPSIG(stat) || SIGSTOP == WSTOPSIG(stat))*/
+		/*If process was stopped add it to jobs[]*/
 		if (WIFSTOPPED(stat)) {
 			job->status = STOPPED;
 			push_job(job);
 			return;
 		} else if (!((WIFEXITED(stat)) || (WIFSIGNALED(stat)))) {
-			goto CHECK; //If fg proccess is still "alive", we should coninue waiting it
+			goto CHECK; /*If fg proccess is still "alive", we should coninue waiting it*/
 		}
 	}
 	else {
 		perror("Couldn't wait for child process termination");
 	}
+	/*If last process in pipeleine finished kill all group*/
 	kill(-job->job_pids[0], SIGKILL);
 	size_t i;
 	for (i = 0; i < job->size-1; i++) {
@@ -145,6 +164,7 @@ void jobs_bg(char ** args) {
 		while (jobs[k].status == RUNNING && k > 0) {
 			k--;
 		}
+		/*Start jobs with given indexes in background*/
 		if (jobs[k].status == STOPPED) {
 			jobs[k].status = RUNNING;
 			printf("[%ld]+\t%s &\n", k+1, jobs[k].job_title);
@@ -162,15 +182,19 @@ void wait_bg() {
 	pop_done();
 	size_t i;
 	int stat;
+	/*Wait for running jobs finish*/
 	for (i = 0; i < job_count; i++) {
 		pid_t pid = -1;
 		if(jobs[i].status == RUNNING){
 			pid = waitpid(jobs[i].job_pids[jobs[i].size-1] ,&stat, WNOHANG);
 		}else{
+			/*And check if some stopped jobs had terminated*/
 			check_for_termination(&jobs[i]);
 			continue;
 		}
+		/*pid>0 means that we have finished process*/
 		if (pid > 0){
+			kill(-jobs[i].job_pgid, SIGKILL);
 			size_t j;
 			for (j = 0; j < jobs[i].size - 1; j++) {
 				waitpid(jobs[i].job_pids[j], 0, 0);
--- shell.h
+++ shell.h
@@ -1,12 +1,30 @@
 #include <sys/types.h>
 
 
-
+/*Max amount of arguments per command*/
 #define MAXARGS 256
+/*Max amount of commands in line*/
 #define MAXCMDS 50
+/*Max amount of jobs (Stopped or running in background)*/
 #define MAXJOBS 20
+/*Max length of job's "title"
+example of output format:
+[./sh] jobs
+#N      JOB STATUS      JOB TITLE
+[1]     Stopped         find ../../ -name *.c | xargs cat | grep 1 | less
+[2]     Stopped         sleep 20
+[3]     Running         sleep 40 | sleep 40 | sleep 40
+*/
 #define MAXTITLE 1024
 
+/*
+cmdflag - for pipe handling (==OUTPIPE for the first process in pipeline /#\ ==INIPIPE for the seconds process in pipelene /#\
+                             ==OUTPIPE|INPIPE for all others members of pipeline)
+bkgrnd  - background flag   (==0 for foreground command /#\ ==1 for background command)
+infile  - input filename            <
+outfile - output&replace filename   >
+appfile - output&append filename    >>
+*/
 typedef struct command
 {
     char *cmdargs[MAXARGS];
@@ -17,10 +35,20 @@ typedef struct command
     char *appfile;
 } cmd_t;
 
+
+/*JOB STATUS defines*/
 #define STOPPED 10
 #define RUNNING 11
 #define DONE 0
 
+/*
+job_title   -   containes shell command which led to job execution
+job_pids    -   PIDs of all processes in pipeline in the same order as they were in command
+job_pgid    -   process group ID of all processes in job
+size        -   amount of PIDs in job_pids (usually equal to amount of procceses in job and group)
+index       -   job index in job control system (to handle multiple fg\bg calls to the same job)
+status      -   shows wheter job is stopped, running, or done
+*/
 typedef struct _job
 {
 	char job_title[MAXTITLE];
@@ -31,12 +59,13 @@ typedef struct _job
 	int status;
 } job_t;
 
+
 /*  cmdflag's  */
 #define OUTPIP  01
 #define INPIP   02
 
 extern struct command cmds[];
-//extern char *infile, *outfile, *appfile;
+/*extern char *infile, *outfile, *appfile;*/
 extern char bkgrnd;
 extern job_t jobs[];
 extern size_t job_count;
@@ -44,15 +73,26 @@ int parseline(char *);
 int promptline(char *, char *, int);
 
 
-//job control unit
-void wait_job(job_t*);
-void wait_bg();
-void add_to_title(char *, char **, char);
-void init_title(char *, char **, char);
-int pop_job(job_t*);
-int push_job(job_t*);
-int get_job(job_t*, int);
-void jobs_bg(char **);
-void print_jobs(cmd_t);
-void pop_done();
-int check_for_termination(job_t*);
+/*job control unit*/
+
+void wait_job(job_t*);/*Waits job in foreground. In case of SIGTSTP moves shell back to foreground and pushes job to jobs[]*/
+
+void wait_bg();/*Waits Running background jobs*/
+
+void add_to_title(char *, char **, char);/*Adds string to title*/
+
+void init_title(char *, char **, char);/*Initiates title with given string*/
+
+int pop_job(job_t*);/*remove job from jobs[]*/
+
+int push_job(job_t*);/*push job to jobs[]*/
+
+int get_job(job_t*, int);/*get job from jobs[] at specific index (fg command handling)*/
+
+void jobs_bg(char **);/*Handles bg command*/
+
+void print_jobs(cmd_t);/*Print list of jobs*/
+
+void pop_done();/*Remove terminated and done jobs from jobs[]*/
+
+int check_for_termination(job_t*);/*Check job for abnormal termination (WIFSIGNALED)*/
